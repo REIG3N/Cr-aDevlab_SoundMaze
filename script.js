60598bbc85de992dc739e5d65251758f0d972527
Ateios <ateosnochamploo@gmail.com>
2023-03-19 14:41:52 +0100

    adding the moving functionality with JavaScript

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
-
 // récupérer la div "character" par son id
+const container = document.getElementById("maze");
 const character = document.getElementById('character');
 
-// définir la position de départ
-let positionX = 0;
-let positionY = 0;
+let currentLigneCaracter = 1;
+let currentColonneCaracter = 2;
+
+function getPositionCaracter(ligne, colonne){
+    let cadreGame = container.getBoundingClientRect();
+    return {positionLeft:(cadreGame.width/16 )*(colonne - 1), positionTop:(cadreGame.height/8)*(ligne - 1)};
+}
+
+function setPositionCarracter(ligne, colonne){
+   let newPosition = getPositionCaracter(ligne, colonne);
+   character.style.top = newPosition.positionTop + 'px';
+   character.style.left = newPosition.positionLeft + 'px';
+}
+
+setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
 
 // ajouter un écouteur d'événements pour détecter les touches fléchées
 document.addEventListener('keydown', (event) => {
     // déplacer la div selon la touche appuyée
     switch (event.key) {
-        case 'ArrowLeft':
-            positionX -= 80;
+
+        case 'ArrowLeft':{
+            currentColonneCaracter -= 1;
+            let key = ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
+            let elt = document.querySelector(key);
+            if(elt != null && elt.classList.contains("groundCase")){
+               setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
+            }else{
+               currentColonneCaracter += 1;
+            }
             break;
-        case 'ArrowUp':
-            positionY -= 80;
+        }
+        case 'ArrowUp':{
+            currentLigneCaracter -= 1;
+            let key = ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
+            let elt = document.querySelector(key);
+            if(elt != null && elt.classList.contains("groundCase")){
+               setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
+            }else{
+               currentLigneCaracter += 1;
+            }
             break;
-        case 'ArrowRight':
-            positionX += 80;
+        }
+        case 'ArrowRight':{
+            currentColonneCaracter +=1;
+            let key = ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
+            let elt = document.querySelector(key);
+            if(elt != null && elt.classList.contains("groundCase")){
+               setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
+            }else{
+               currentColonneCaracter -= 1;
+            }
             break;
-        case 'ArrowDown':
-            positionY += 80;
+        }
+        case 'ArrowDown':{
+            currentLigneCaracter += 1;
+            let key = ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
+            let elt = document.querySelector(key);
+            if(elt != null && elt.classList.contains("groundCase")){
+               setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
+            }else{
+               currentLigneCaracter -= 1;
+            }
             break;
+        }
         default:
             break;
     }
-    // mettre à jour la position de la div
-    character.style.top = positionY + 'px';
-    character.style.left = positionX + 'px';
 });
