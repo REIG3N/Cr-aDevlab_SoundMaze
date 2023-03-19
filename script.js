4c168dff6037ce5e6ccfd7edecc3aa4ce2f64e0a
MEHDI BELLAM <mehdi.bellam@outlook.fr>
2023-03-19 16:02:59 +0000

    :sparkles: le bloc se déplace bien grace a la voix :sparkles:

--- script.js
+++ script.js
@@ -24,12 +24,99 @@ setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
 
 console.log("ml5 version:", ml5.version);
 
+
+
 // Initialize a sound classifier method with SpeechCommands18w model. A callback needs to be passed.
 // Options for the SpeechCommands18w model, the default probabilityThreshold is 0
 const classifier = ml5.soundClassifier("SpeechCommands18w", modelReady);
 
 // Options for the SpeechCommands18w model, the default probabilityThreshold is 85
-const options = { probabilityThreshold: 0.85 };
+const options = { probabilityThreshold: 0.99 };
+
+
+setTimeout(gotResult, 1000);
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+        
+      // Display error in the console
+    
+      if (error) {
+        console.error(error);
+      }
+      // The results are in an array ordered by confidence.
+      switch (results[0].label) {
+        
+          case "left": {
+              console.log(results[0].label);
+              console.log(results[0].confidence);
+          currentColonneCaracter -= 1;
+          let key =
+            ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
+          let elt = document.querySelector(key);
+          if (elt != null && elt.classList.contains("groundCase")) {
+            setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
+          } else {
+            currentColonneCaracter += 1;
+          }
+          break;
+        }
+          case "up": {
+              console.log(results[0].label);
+              console.log(results[0].confidence);
+          currentLigneCaracter -= 1;
+          let key =
+            ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
+          let elt = document.querySelector(key);
+          if (elt != null && elt.classList.contains("groundCase")) {
+            setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
+          } else {
+              console.log(results[0].label);
+              console.log("break")
+              
+            currentLigneCaracter += 1;
+          }
+          break;
+        }
+          case "right": {
+              console.log(results[0].label);
+              console.log(results[0].confidence);
+          currentColonneCaracter += 1;
+          let key =
+            ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
+          let elt = document.querySelector(key);
+          if (elt != null && elt.classList.contains("groundCase")) {
+            setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
+          } else {
+            currentColonneCaracter -= 1;
+          }
+          break;
+        }
+          case "down": {
+              console.log(results[0].label);
+              console.log(results[0].confidence);
+          currentLigneCaracter += 1;
+          let key =
+            ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
+          let elt = document.querySelector(key);
+          if (elt != null && elt.classList.contains("groundCase")) {
+            setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
+          } else {
+            currentLigneCaracter -= 1;
+          }
+          break;
+        }
+        default:
+              break;
+          
+      }
+    }
+
+
+
+function modelReady() {
+  // classify sound
+  classifier.classify(gotResult);
+}
 // Two variable to hold the label and confidence of the result
 let label;
 let confidence;
@@ -37,7 +124,7 @@ let confidence;
 
 function preload() {
     // Load SpeechCommands18w sound classifier model
-// -82,21 +82,53 @@ function preload() {
+    // -82,21 +82,53 @@ function preload() {
     function setup() {
         noCanvas();
         // Create 'label' and 'confidence' div to hold results
@@ -47,30 +134,10 @@ function preload() {
         // Classify the sound from microphone in real time
         classifier.classify(gotResult);
     }
+}
 
-    function modelReady() {
-        // classify sound
-        classifier.classify(gotResult);
-    }
+   
 
-
-// A function to run when we get any errors and the results
-    function gotResult(error, results) {
-        // Display error in the console
-        if (error) {
-            console.error(error);
-        }
-        // The results are in an array ordered by confidence.
-        console.log(results[0]);
-
-        console.log(results[0]);
-        return results[0];
-
-        // Show the first label and confidence
-        label.html('Label: ' + results[0].label);
-        confidence.html('Confidence: ' + nf(results[0].confidence, 0, 2)); // Round the confidence to 0.01
-
-    }}
 
 
 
@@ -78,55 +145,8 @@ function preload() {
 
 // ajouter un écouteur d'événements pour détecter les touches fléchées
 function SoundMovement() {
-    switch (gotResult()){
-        case 'left':{
-            currentColonneCaracter -= 1;
-            let key = ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
-            let elt = document.querySelector(key);
-            if(elt != null && elt.classList.contains("groundCase")){
-                setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
-            }else{
-                currentColonneCaracter += 1;
-            }
-            break;
-        }
-        case 'up':{
-            currentLigneCaracter -= 1;
-            let key = ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
-            let elt = document.querySelector(key);
-            if(elt != null && elt.classList.contains("groundCase")){
-                setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
-            }else{
-                currentLigneCaracter += 1;
-            }
-            break;
-        }
-        case 'right':{
-            currentColonneCaracter +=1;
-            let key = ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
-            let elt = document.querySelector(key);
-            if(elt != null && elt.classList.contains("groundCase")){
-                setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
-            }else{
-                currentColonneCaracter -= 1;
-            }
-            break;
-        }
-        case 'down':{
-            currentLigneCaracter += 1;
-            let key = ".item_" + currentLigneCaracter + "" + currentColonneCaracter;
-            let elt = document.querySelector(key);
-            if(elt != null && elt.classList.contains("groundCase")){
-                setPositionCarracter(currentLigneCaracter, currentColonneCaracter);
-            }else{
-                currentLigneCaracter -= 1;
-            }
-            break;
-        }
-        default:
-            break;
-    }
+    
 }
 
-SoundMovement();
+
 
